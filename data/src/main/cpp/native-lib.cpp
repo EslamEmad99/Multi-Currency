15b9bab2d92a11bbef5581c683aab35e3d449153
Eslam Emad <eslam.eali.ext@orange.com>
2025-03-28 08:09:27 +0200

    - Change access_key

--- data/src/main/cpp/native-lib.cpp
+++ data/src/main/cpp/native-lib.cpp
@@ -3,6 +3,6 @@
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_data_util_Secrets_getApiKey(JNIEnv* env, jobject /* this */) {
-    std::string api_key = "your_actual_api_key_here";  // Replace with your real API key
+    std::string api_key = "0848ee203030487f8c58494aa064df4c";  // Replace with your real API key
     return env->NewStringUTF(api_key.c_str());
 }
